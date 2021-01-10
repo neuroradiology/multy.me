d5c23104154abc0043536093f09233a199cc4cae
Michael <etheanore@gmail.com>
2021-01-10 22:04:14 +0100

    Add panelbear analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../style/main.css'
+import { usePanelbear } from "./../hooks/panelbear";
 
-function MyApp({ Component, pageProps }) {
+function CustomApp({ Component, pageProps }) {
+  // Load Panelbear only once during the app lifecycle
+  usePanelbear("I7onXLRKXb2", {
+    // Uncomment to allow sending events on localhost, and log to console too.
+    // debug: true
+  });
+
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default CustomApp;
+
+
