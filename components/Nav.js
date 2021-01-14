23bf52359df682c09658cbf079c46af3960e1175
Michael <etheanore@gmail.com>
2021-01-14 14:56:10 +0100

    Fix logo position

--- components/Nav.js
+++ components/Nav.js
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function Nav() {
   return (
     <nav className='font-title'>
-      <div className='text-2xl text-white object-scale-down  ml-5 mt-3'>
+      <div className='text-2xl text-white object-scale-down ml-5'>
         <Link href='/'>
           <a>
-            <Image src='/logo.svg' alt='multy.me' width={280} height={115.2} />
+            <Image src='/logo.svg' alt='multy.me' width={252} height={103.7} />
           </a>
         </Link>
       </div>
