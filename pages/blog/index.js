2e2a8ce25b08de6cf47a8f6e4b8620eb110170fa
Michael <etheanore@gmail.com>
2021-01-16 22:25:30 +0100

    First blog integration
    Need to clean components, merge the component folder, and delete some unused assets.
    Need to update some constant (lib/constants for ex)

---
+++ pages/blog/index.js
@@ -0,0 +1,48 @@
+import Container from "components/Blog/container";
+import MoreStories from "components/Blog/more-stories";
+import HeroPost from "components/Blog/hero-post";
+import Layout from "components/Layout";
+import { getAllPosts } from "lib/api";
+import Head from "next/head";
+
+export default function Index({ allPosts }) {
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Multy.me | Blog</title>
+        </Head>
+        <Container>
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const allPosts = getAllPosts([
+    "title",
+    "date",
+    "slug",
+    "author",
+    "coverImage",
+    "excerpt",
+  ]);
+
+  return {
+    props: { allPosts },
+  };
+}
