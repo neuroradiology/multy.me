c239233d61aed121b5821905bfe374c50db9aff2
Michael <etheanore@gmail.com>
2021-01-18 16:57:48 +0100

    Improve blog design + add link on the navbar

--- components/Blog/posts.js
+++ components/Blog/posts.js
@@ -1,12 +1,15 @@
-import PostPreview from './post-preview'
+import PostPreview from "./post-preview";
+import Image from "next/image";
 
 export default function MoreStories({ posts }) {
   return (
-    <section>
-      <h2 className="mb-8 text-3xl md:text-4xl font-bold tracking-tighter leading-tight">
-        Blog and Changelog
-      </h2>
-      <div className="flex flex-col flex-wrap">
+    <section className="mt-5">
+     <div className="flex flex-col"> <Image src='/type.png' width={112*0.8} height={126*0.8} className="m-auto text-center object-contain" /><h2 className='mb-2 text-3xl md:text-4xl font-base text-center tracking-tighter leading-tight uppercase'>
+        Blog {"&"} Changelog
+      </h2></div>
+      
+
+      <div className='flex flex-col flex-wrap'>
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
@@ -20,5 +23,5 @@ export default function MoreStories({ posts }) {
         ))}
       </div>
     </section>
-  )
+  );
 }
--- components/Nav.js
+++ components/Nav.js
@@ -2,15 +2,26 @@ import Link from "next/link";
 import Image from "next/image";
 export default function Nav() {
   return (
-    <nav className='font-title flex flex-row flex-wrap mx-5 '>
-      
-        <Link href='/' >
+    <nav className='font-title flex flex-row flex-wrap mx-5 content-center '>
+      <div className='flex-grow'>
+        <Link href='/'>
           <a>
-            <Image className="ml-5" src='/logo.svg' alt='multy.me' width={252} height={103.7} />
+            <Image
+              className='ml-5'
+              src='/logo.svg'
+              alt='multy.me'
+              width={252}
+              height={103.7}></Image>
           </a>
         </Link>
-        
-      
+      </div>
+      <div
+        id='menu'
+        className='uppercase justify-self-end sm:my-auto m-auto sm:m-0  hover:text-blue-600'>
+        <Link href='/blog'>
+          <a className='my-auto'>Blog</a>
+        </Link>
+      </div>
     </nav>
   );
 }
