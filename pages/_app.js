aaebd0edd6a626cc463376367994e022924af3d3
Michael <etheanore@gmail.com>
2021-01-19 17:52:23 +0100

    Fixed no follow issue (SEO)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '../style/main.css'
+import "../style/main.css";
 import { usePanelbear } from "./../hooks/panelbear";
+import { DefaultSeo } from "next-seo";
 
 function CustomApp({ Component, pageProps }) {
   // Load Panelbear only once during the app lifecycle
@@ -8,9 +9,23 @@ function CustomApp({ Component, pageProps }) {
     // debug: true
   });
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          url: "https://www.multy.me/",
+          site_name: "Multy.me",
+        }}
+        twitter={{
+          handle: "@michael_webdev",
+          site: "@michael_webdev",
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default CustomApp;
-
-
--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
     <Layout>
       <Head>
         <title>Multy.me</title>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        <meta name="description" content="The multi URL shortener" />
+
+        <meta name='description' content='The multi URL shortener' />
       </Head>
       <div>
         <div className='font-main'>
