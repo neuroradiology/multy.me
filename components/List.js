f063013decf4a67e6f6b6aef5533f35d03fe62c7
Michael <etheanore@gmail.com>
2021-01-20 22:58:57 +0100

    Add thumbnail system to show the url list with a better visual

---
+++ components/List.js
@@ -0,0 +1,16 @@
+export default function List(props) {
+  return (
+    <div className='flex flex-auto flex-shrink flex-wrap flex-row m-auto justify-evenly '>
+      {props.result.urls.map((el, index) => (
+        <div key={index} className='w-auto m-5'>
+          <a
+            href={el.url}
+            rel='external nofollow'
+            className='text-blue-600 hover:text-blue-800'>
+            {el.url}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
