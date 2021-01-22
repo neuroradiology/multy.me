3cac3139b73ad4cf50baee73a21f5cb42d474518
Michael <etheanore@gmail.com>
2021-01-22 14:20:27 +0100

    Add Product Hunt link

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,27 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer className='flex flex-wrap m-3 justify-around'>
-      <div className="m-auto">
+      <div className='m-auto'>
         <a href='https://shreyasdeshpande.com/'>
           Designed by Shreyas Deshpande
         </a>
       </div>
-      <div  className='flex-grow text-center m-auto'>
+      <div className='flex-grow text-center m-auto'>
         <p>© Multy.me</p>
       </div>
-      <div className='max-w-xxs object-contain object-right-bottom mr-0 order-last'>
-        <a className='' href='https://ko-fi.com/Y8Y41LNMX'>
-          <Image src='/kofi2.png' alt="buy me a coffee" width={160} height={40.3} />
+      <div className=' object-contain object-right-bottom mr-0 order-last'>
+        <a
+          href='https://www.producthunt.com/posts/multy?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-multy'
+          target='_blank'>
+          <img
+            src='https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=281938&theme=dark'
+            alt='Multy - Generate a short URL 🔗 to share a list of websites | Product Hunt'
+            style={{width: '250px', height: '54px'}}
+            width='250'
+            height='54'
+          />
         </a>
       </div>
     </footer>
